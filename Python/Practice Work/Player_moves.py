6571133f9b3f5166ff2d86b83638854bc09f83d4
Usanda Mkhaliphi <164313294+usandamkhaliphi@users.noreply.github.com>
2026-09-16 09:42:39 +0200

    Added Player_moves.py

---
+++ Python/Practice Work/Player_moves.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+import random  
+
+class Player(ABC):
+    def __init__(self):
+        self.moves = []
+        self.position = (0, 0)
+        self.path = [self.position] 
+    
+    def make_move(self):
+        chosen_move = random.choice(self.moves)
+        
+        new_x = self.position[0] + chosen_move[0]
+        new_y = self.position[1] + chosen_move[1]
+        self.position = (new_x, new_y)
+
+        self.path.append(self.position)
+        return self.position
+
+    @abstractmethod
+    def level_up(self):
+        pass
+    
+class Pawn(Player):
+    def __init__(self):
+        super().__init__()
+        self.moves = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+    
+    def level_up(self):
+        diagonal_moves = [(1, 1), (-1, 1), (1, -1), (-1, -1)]
+        self.moves.extend(diagonal_moves)
